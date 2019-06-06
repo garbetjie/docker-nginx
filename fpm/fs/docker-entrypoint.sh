e3105de3059b23cd1f765e45ff9d8084f18cd4e5
Geoff Garbers <geoff@garbers.co.za>
2019-06-06 13:28:00 +0200

    Rename folder, and update default variables.

--- fpm/fs/docker-entrypoint.sh
+++ fpm/fs/docker-entrypoint.sh
@@ -5,10 +5,7 @@ envsubst '$FCGI_HOST
           $FCGI_PORT
           $MAX_REQUEST_SIZE
           $WEBROOT
-          $TIMEOUT' < /etc/nginx/nginx.conf > /etc/nginx/.nginx.conf
-
-# Move the updated file.
-mv /etc/nginx/.nginx.conf /etc/nginx/nginx.conf
+          $TIMEOUT' < /etc/nginx/nginx.conf | tee /etc/nginx/nginx.conf 1> /dev/null
 
 # Run the nginx server.
 exec nginx
