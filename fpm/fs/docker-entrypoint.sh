cfd3e5d5614716fd5bd3403db033f65e0b6cc778
Geoff Garbers <geoff@garbers.co.za>
2019-06-07 11:45:42 +0200

    Add status endpoint handling for FPM.

--- fpm/fs/docker-entrypoint.sh
+++ fpm/fs/docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 # Replace environment variables in nginx configuration file.
-envsubst '$FCGI_HOST
-          $FCGI_PORT
-          $MAX_REQUEST_SIZE
-          $WEBROOT
-          $TIMEOUT' < /etc/nginx/nginx.conf | tee /etc/nginx/nginx.conf 1> /dev/null
+IFS=$'\n'
+for file in `find /etc/nginx -type f -iname '*.conf'`; do
+    envsubst '$FCGI_HOST
+              $FCGI_PORT
+              $MAX_REQUEST_SIZE
+              $WEBROOT
+              $TIMEOUT' < "$file" | tee "$file" 1> /dev/null
+done
 
 # Run the nginx server.
 exec nginx
