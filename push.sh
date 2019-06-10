8581d8de4985c8af810e2140412e6c42ae2c5d41
Geoff Garbers <geoff@garbers.co.za>
2019-06-10 06:36:40 +0200

    Make build async.

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ $# -lt 2 ]]; then
     printf "Usage: $0 \e[4mGCP_PROJECT\e[0m \e[4mDOCKER_HUB_REPO\e[0m\n"
     exit 1
@@ -13,7 +15,22 @@ cd "$(cd "$(dirname "$0")" && pwd)"
 
 # Submit the build.
 printf "\n\e[38;5;116mSubmitting build.\e[0m\n"
-gcloud builds submit \
+
+build_id="$(
+    gcloud builds submit \
+    --async \
+    --format "value(id)" \
     --project "$project" \
     --substitutions "_DOCKER_HUB_REPO=${docker_hub_repo}" \
-    --config ./cloudbuild.yaml .
+    --config ./cloudbuild.yaml .)"
+
+printf "\nUsing build ID ${build_id}.\n"
+printf "Do you want to follow the build logs? [Y/n] "
+read confirm
+
+if [[ "$confirm" = "Y" ]] || [[ "$confirm" = "y" ]] || [[ "$confirm" = "" ]]; then
+    gcloud builds log "$build_id" --stream
+else
+    printf "To follow the build logs, run the following command:\n\n"
+    printf "  \e[3mgcloud builds log ${build_id} --stream\e[0m\n"
+fi
