daba8524ee45547ad6859245f4fd7284da8ca4aa
Geoff Garbers <geoff@garbers.co.za>
2019-06-12 07:09:46 +0200

    Change FCGI_* to FPM_*. Add ability to customise the FPM status path and allowed/denied hosts that can access it.

--- fpm/fs/docker-entrypoint.sh
+++ fpm/fs/docker-entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+# Build up the list of hosts that are allowed to access the FPM status path.
+IFS=" "
+fpm_status_hosts_formatted=""
+
+for cidr in $FPM_STATUS_HOSTS_ALLOWED; do
+    [[ "$cidr" != "" ]] && fpm_status_hosts_formatted="${fpm_status_hosts_formatted}            allow ${cidr};"$'\n'
+done
+
+for cidr in $FPM_STATUS_HOSTS_DENIED; do
+    [[ "$cidr" != "" ]] && fpm_status_hosts_formatted="${fpm_status_hosts_formatted}            deny ${cidr};"$'\n'
+done
+
+export FPM_STATUS_HOSTS_FORMATTED="$fpm_status_hosts_formatted"
+
 # Replace environment variables in nginx configuration file.
 IFS=$'\n'
 for file in `find /etc/nginx -type f -iname '*.conf'`; do
-    envsubst '$FCGI_HOST
-              $FCGI_PORT
+    envsubst '$FPM_HOST
+              $FPM_PORT
+              $FPM_STATUS_PATH
+              $FPM_STATUS_HOSTS_FORMATTED
               $MAX_REQUEST_SIZE
               $WEBROOT
               $TIMEOUT' < "$file" | tee "$file" 1> /dev/null
